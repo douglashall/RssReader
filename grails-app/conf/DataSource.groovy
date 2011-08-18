68538fe75a8b18c3f1307ab6c9d464ef21427829
Douglas Hall <douglas_hall@harvard.edu>
2011-08-18 16:40:25 -0400

    configured jndi datasource

---
+++ grails-app/conf/DataSource.groovy
@@ -0,0 +1,33 @@
+dataSource {
+    pooled = true
+    driverClassName = "org.hsqldb.jdbcDriver"
+    username = "sa"
+    password = ""
+}
+hibernate {
+    cache.use_second_level_cache = true
+    cache.use_query_cache = true
+    cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
+}
+// environment specific settings
+environments {
+    development {
+        dataSource {
+            dbCreate = "update" // one of 'create', 'create-drop','update'
+			dialect = "org.hibernate.dialect.OracleDialect"
+            jndiName = "java:comp/env/jdbc/icbDS"
+        }
+    }
+    test {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:hsqldb:mem:testDb"
+        }
+    }
+    production {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:hsqldb:file:prodDb;shutdown=true"
+        }
+    }
+}
